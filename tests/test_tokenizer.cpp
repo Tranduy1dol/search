33838d169f3bf14449ac0c9ce548f397cd957728
Tranduy1dol <tmd.id2004@gmail.com>
2026-05-13 22:20:44 +0700

    fix: test

--- tests/test_tokenizer.cpp
+++ tests/test_tokenizer.cpp
@@ -4,32 +4,35 @@
 
 #include "search/indexer/tokenizer.h"
 
+#ifndef TEST_DATA_DIR
+#define TEST_DATA_DIR "."
+#endif
+
+static const std::string kStopwordsPath =
+    std::string(TEST_DATA_DIR) + "/stopwords_vi.txt";
+
 TEST(TokenizeTest, NoStopwordRemoved) {
-  search::Tokenizer tokenizer(
-      "/home/tranduy/Workspace/search/data/stopwords_vi.txt");
+  search::Tokenizer tokenizer(kStopwordsPath);
   std::vector<std::string> expected{"hello", "world"};
 
   EXPECT_EQ(tokenizer.Tokenize("Hello World"), expected);
 }
 
 TEST(TokenizeTest, StopwordRemoved) {
-  search::Tokenizer tokenizer(
-      "/home/tranduy/Workspace/search/data/stopwords_vi.txt");
+  search::Tokenizer tokenizer(kStopwordsPath);
   std::vector<std::string> expected{"khó", "vl"};
 
   EXPECT_EQ(tokenizer.Tokenize("tôi khó vl"), expected);
 }
 
 TEST(IsStopwordTest, IsStopword) {
-  search::Tokenizer tokenizer(
-      "/home/tranduy/Workspace/search/data/stopwords_vi.txt");
+  search::Tokenizer tokenizer(kStopwordsPath);
 
   EXPECT_TRUE(tokenizer.IsStopword("và"));
 }
 
 TEST(IsStopwordTest, NotStopword) {
-  search::Tokenizer tokenizer(
-      "/home/tranduy/Workspace/search/data/stopwords_vi.txt");
+  search::Tokenizer tokenizer(kStopwordsPath);
 
   EXPECT_FALSE(tokenizer.IsStopword("tao"));
 }
